9bb6c7803cfd1724bff78fa7849de6838bac7c33
LucianoAndresLugani <63163767+LucianoAndresLugani@users.noreply.github.com>
2023-04-26 19:29:31 -0300

    quinta tarea

---
+++ quinta-tarea/numeros.js
@@ -0,0 +1,8 @@
+//tarea de numeros
+const alturaCm = 165;
+const alturaMetros = alturaCm / 100;
+const pesoKg = 60.5;
+const alturaRedondeada = Math.ceil(alturaMetros);
+const pesoRedondeado = Math.floor(pesoKg);
+const maximoValor = Number.MAX_VALUE;
+const comparacionMaximo = (maximoValor + 1) === maximoValor;
