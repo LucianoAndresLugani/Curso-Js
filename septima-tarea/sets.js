ad6e5b582a437c4aae52e9cbb5803adb20c239dc
LucianoAndresLugani <63163767+LucianoAndresLugani@users.noreply.github.com>
2023-04-30 23:28:37 -0300

    septima tarea

---
+++ septima-tarea/sets.js
@@ -0,0 +1,9 @@
+// Crear un nuevo Set con los nombres de tu familia
+const nombresFamilia = new Set(["Ana", "Luis", "María", "Pedro"]);
+console.log(nombresFamilia)
+// Modificar el Set original añadiendo tu nombre (duplicado)
+nombresFamilia.add("Luis");
+console.log(nombresFamilia)
+// Modificar el Set original añadiendo el nombre "Javascript"
+nombresFamilia.add("Javascript");
+console.log(nombresFamilia)
